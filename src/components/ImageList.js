5429a26a09bf01d6ee1d390fc5143044382653ef
AbdussamadYisau <sammieyisau@gmail.com>
2021-02-04 20:06:40 +0100

    100% done

---
+++ src/components/ImageList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
+
+const ImageList = (props) => {
+    const images = props.images.map((image) => {
+        return <ImageCard  image={image}/>
+    });
+
+    return <div className="image-list">{images}</div>
+}
+
+export default ImageList;
